d750fda56194443d2829973bd6080f338f163817
Maxim <www.bigstar.promax@gmail.com>
2024-03-13 09:09:20 +0700

    Добавлен composer

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maxpolka/pop-it-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e10a81089b1cfeda91ecc0e258675ecee2307954',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'maxpolka/pop-it-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e10a81089b1cfeda91ecc0e258675ecee2307954',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
