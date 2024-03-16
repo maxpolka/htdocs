ba455743a7d3806919408bb7bd0198c897150e59
Maxim <www.bigstar.promax@gmail.com>
2024-03-16 11:40:50 +0700

    Добавлены модели

--- pop-it-mvc/vendor/composer/autoload_real.php
+++ pop-it-mvc/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit68e480389071d4d2c4e5659ebf7ca504
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit68e480389071d4d2c4e5659ebf7ca504::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
